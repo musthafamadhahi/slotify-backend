89fcb89e55752560b7fc3d9b51d8c3cda07d8345
Mujithaba <mujithaba2001@gmail.com>
2025-09-04 13:27:06 +0530

    firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,21 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_JSON
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
-  : {
-      type: process.env.FIREBASE_TYPE,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // fix newlines
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      clientId: process.env.FIREBASE_CLIENT_ID,
-      authUri: process.env.FIREBASE_AUTH_URI,
-      tokenUri: process.env.FIREBASE_TOKEN_URI,
-      authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-      universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-    };
+const serviceAccount: ServiceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+};
 
 if (!serviceAccount) {
   throw new Error('FIREBASE_SERVICE_ACCOUNT_JSON is not defined');
